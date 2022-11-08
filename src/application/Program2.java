cf64ef4626948798ed3de2d02497c909690e4d9e
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-07 21:29:00 -0300

    feat: implemention update

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,17 +1,33 @@
 package application;
 
-import java.util.Date;
+import java.util.Scanner;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("\n=== TEST 4: seller insert ===");
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao department = new DaoFactory().createDepartmentDao();
+		
+		System.out.println("\n=== TEST 1: department insert ===");
 		Department dep = new Department(3, "Fotball");
 		System.out.println("Inserted! New id = " + dep.getId());
+		
+		System.out.println("\n=== TEST 2: department delete ===");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		
+		System.out.println("\n=== TEST 3: department update ===");
+		dep = department.findById(1);
+		dep.setName("Actor");
+		department.update(dep);
+		System.out.println("Update completed");
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -30,7 +30,24 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+ "SET Id = ?, Name = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);;
+		}
 		
 	}
 
