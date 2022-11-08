65b01b2151e47a3ebc392bf9b63497e9e492811d
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-08 14:51:19 -0300

    feat: implemention findAll

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -36,6 +36,12 @@ public class Program2 {
 		departmentDao.update(department);
 		System.out.println("Update completed");
 		
+		System.out.println("\n=== TEST 5: seller findAll ===");
+		List <Department> list = departmentDao.findAll();
+		for (Department d : list) {
+			System.out.println(d);
+		}	
+		
+		sc.close();
 	}
-
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -126,8 +129,31 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT * FROM department ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Department> list = new ArrayList<>();
+			
+			while (rs.next()) {
+					Department obj = new Department();
+					obj.setId(rs.getInt("Id"));
+					obj.setName(rs.getString("Name"));
+					list.add(obj);
+			}
+			return list;
+		}
+		catch(SQLException e ) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
